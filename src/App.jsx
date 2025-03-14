7c929ac7294f9151892d1d75d7b73733809790d7
Francisco Pontes <franciscopontes@Mac-Mini-kOTB.lan>
2025-03-14 15:50:35 +0000

    Refactored App.jsx by splitting into separate components and stored them into separate files, inside a folder, for better structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/MainSection";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <header>
-        <h1>Little Lemon</h1>
-      </header>
-
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Menu</a>
-          </li>
-          <li>
-            <a href="#">Reservations</a>
-          </li>
-          <li>
-            <a href="#">Order Online</a>
-          </li>
-        </ul>
-      </nav>
-
-      <main>
-        <h2>Welcome to Little Lemon</h2>
-        <p>Experience the best Mediterranean flavors.</p>
-      </main>
-
-      <footer>
-        <p>&copy; 2025 Little Lemon. All rights reserved.</p>
-      </footer>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
     </>
   );
 }
---
+++ src/components/MainSection.jsx
@@ -0,0 +1,10 @@
+function MainSection() {
+  return (
+    <main>
+      <h2>Welcome to Little Lemon</h2>
+      <p>Experience the best Mediterranean flavors.</p>
+    </main>
+  );
+}
+
+export default MainSection;
